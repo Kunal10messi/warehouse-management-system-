ebe93bd2e37f41a5284049b62fe440a6fa7629fc
Kunal Singh <kunalmessi1302@gmail.com>
2026-03-20 15:15:02 +0530

    final production setup before deploy

--- warehouse_system/settings.py
+++ warehouse_system/settings.py
@@ -70,23 +70,7 @@ WSGI_APPLICATION = 'warehouse_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 
 # Password validation

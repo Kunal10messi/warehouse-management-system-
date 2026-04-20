b97a9fd231f131845a84af625cc424b63bdd38a7
Kunal Singh <kunalmessi1302@gmail.com>
2026-04-20 20:58:13 +0530

    implemented read-only API endpoints using GenericViewSet with nested serializers

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import DeviceViewSet, AssignmentViewSet
 
-urlpatterns = [
-    # empty for now
-]
+router = DefaultRouter()
+
+router.register('devices', DeviceViewSet, basename='device')
+router.register('assignments', AssignmentViewSet, basename='assignment')
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from inventory.models import Device
+from allocations.models import Assignment
+from .serializers import DeviceSerializer, AssignmentSerializer
 
-# Create your views here.
+class DeviceViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class AssignmentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = Assignment.objects.select_related('user', 'device')
+    serializer_class = AssignmentSerializer
+    
+    
+    
